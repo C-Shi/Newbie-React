169c93f563f3ad8f73b929e65b7b3e398df650b4
Cheng <cshi@ualberta.ca>
2023-03-19 13:57:22 -0600

    change title

--- src/Navigation.js
+++ src/Navigation.js
@@ -12,7 +12,7 @@ export default function Navigation(props) {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand logo" href="#">Advanced React</a>
+            <a className="navbar-brand logo" href="#">Advanced React Oct 17, 2022</a>
           </div>
           <div className="collapse navbar-collapse" id="myNavbar">
             <ul className="nav navbar-nav navbar-right">
