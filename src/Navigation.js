2eab603189d6e30d1267a8b822ef466a6e34106e
Cheng <cshi@ualberta.ca>
2024-03-19 19:55:09 -0600

    break the test

--- src/Navigation.js
+++ src/Navigation.js
@@ -17,7 +17,7 @@ export default function Navigation(props) {
             <ul className="nav navbar-nav navbar-right">
               <li data-testid="home-icon"><Link to="/">Home</Link></li>
               <li data-testid="about-icon"><Link to="/about">About</Link></li>
-              <li data-testid="connect-icon"><Link to="/connect">Connect</Link></li>
+              <li data-testid="connect-icon"><Link to="/connect">Contact</Link></li>
             </ul>
           </div>
         </div>
